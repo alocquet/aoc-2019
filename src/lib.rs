31508de97cf6bc2eea24c51df524fe8980cb19f0
Arnaud locquet <alocquet@gmail.com>
2019-12-01 00:00:43 +0100

    feat: ci

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn read_file(file_name: &str) -> String {
+    let mut file = File::open(file_name).expect("file not found");
+    let mut result = String::new();
+    file.read_to_string(&mut result).expect("something went wrong reading the file");
+    result
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn read_a_test_file() {
+        let content = read_file("inputs/test.txt");
+        assert_eq!(content, "test");
+    }
+
+    #[test]
+    #[should_panic]
+    fn read_file_which_not_exist_should_panic() {
+        read_file("inputs/dummy.txt");
     }
 }
