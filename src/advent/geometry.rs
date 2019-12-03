59d78c73266ab242afb44ff62d159fd0952c3c96
Arnaud <alocquet@gmail.com>
2019-12-03 12:53:29 +0100

    feat: day 03 (#5)
    feat: day 03

---
+++ src/advent/geometry.rs
@@ -0,0 +1,27 @@
+use std::ops::AddAssign;
+
+pub const UP: Point = Point { x: 0, y: -1 };
+pub const DOWN: Point = Point { x: 0, y: 1 };
+pub const RIGHT: Point = Point { x: 1, y: 0 };
+pub const LEFT: Point = Point { x: -1, y: 0 };
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct Point {
+    pub x: isize,
+    pub y: isize,
+}
+
+impl Point {
+    pub fn manhattan_distance_from(&self, other: &Point) -> usize {
+        (self.x - other.x).abs() as usize + (self.y - other.y).abs() as usize
+    }
+}
+
+impl AddAssign for Point {
+    fn add_assign(&mut self, other: Point) {
+        *self = Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        };
+    }
+}
